a8c814afdf643ee40c60198124f327aa39c492bb
Agath08 <agathiyanjana@gmail.com>
2025-02-11 21:58:34 +0530

    Add files via upload

---
+++ ex2/src/main/java/com/mystore/app/AppConfig.java
@@ -0,0 +1,14 @@
+package com.mystore.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public Product product() {
+		return new Product();
+	}
+	
+}
